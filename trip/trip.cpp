813a79a9c92127a65ce1ce40adfbe56ebc7a7f83
WaiCat <98246479+WaiCat@users.noreply.github.com>
2024-03-18 13:41:14 +0900

    trip 해결

--- trip/trip.cpp
+++ trip/trip.cpp
@@ -1,17 +1,51 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <cmath>
+#include <iomanip>
 
 using namespace std;
 
 int main() {
+    ifstream fin; 
+    ofstream fout;
+    fin.open("trip.inp"); 
+    fout.open("trip.out");
 
-  ifstream fin; ofstream fout;
-  fin.open("3nplus1.inp"); fout.open("3nplus1.out");
 
+    int n;
 
-  fin.close();
-  fout.close(); 
+    while(fin >> n){
+        if(n == 0){
+            break;
+        }
 
-  return 0;
+        double arr[n];
+        double sum = 0.0;
+        double Negative = 0.0;
+        double Positive = 0.0;
+
+        for (int i = 0; i<n; i++){
+            fin >> arr[i];
+            sum += arr[i];
+        }
+
+        sum = sum / n;
+
+        for (int i = 0; i<n; i++){
+            if(sum > arr[i]){
+                Positive += (int)((sum - arr[i]) * 100) / 100.0;
+            } else {
+                Negative += (int)((arr[i] - sum) * 100) / 100.0;
+            }
+        }
+
+        fout << "$" << fixed << setprecision(2) << max(Positive, Negative) << endl;
+
+    }
+
+    fin.close();
+    fout.close(); 
+
+    return 0;
 }
