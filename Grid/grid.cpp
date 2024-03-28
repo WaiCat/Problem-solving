4705cbcced743d486a3fd2458556e7b0fbbf64c5
WaiCat <98246479+WaiCat@users.noreply.github.com>
2024-03-28 10:13:14 +0900

    grid

--- Grid/grid.cpp
+++ Grid/grid.cpp
@@ -1,3 +1,4 @@
+#include <deque>
 #include <fstream>
 #include <iostream>
 #include <vector>
@@ -7,98 +8,101 @@
 using namespace std;
 
 struct Cell {
-  int state;
-  vector<int> possibilities;
+    int state;
+    deque<int> possibilities;
 
-  Cell() : state(0) {}
+    Cell() : state(0) {}
 };
 
-int addMod(int a, int b) { return (a % MOD + b % MOD) % MOD; }
-
 int main() {
-  ifstream fin("0.inp");
-  ofstream fout("0.txt");
+    ifstream fin("2.inp");
+    ofstream fout("2.txt");
 
-  int test_cases;
-  fin >> test_cases;
+    int test_cases;
+    fin >> test_cases;
 
-  for (int t = 0; t < test_cases; ++t) {
-    int rows, cols, a, b, k;
-    fin >> rows >> cols >> a >> b >> k;
+    for (int t = 0; t < test_cases; ++t) {
+        int rows, cols, a, b, k;
+        fin >> rows >> cols >> a >> b >> k;
 
-    vector<vector<Cell>> grid(rows + 1, vector<Cell>(cols + 1));
+        vector<vector<Cell>> grid(rows + 1, vector<Cell>(cols + 1));
 
-    for (int i = 0; i <= rows; ++i) {
-      for (int j = 0; j <= cols; ++j) {
-        grid[i][j].possibilities = vector<int>(k + 1, 0);
-      }
-    }
-
-    for (int i = 0; i < a; ++i) {
-      int x, y;
-      fin >> x >> y;
-      grid[x][y].state = 1;
-    }
-
-    for (int i = 0; i < b; ++i) {
-      int x, y;
-      fin >> x >> y;
-      grid[x][y].state = 2;
-    }
-
-    grid[0][0].possibilities[0] = 1;
-
-    for (int i = 1; i <= rows; ++i) {
-      if (grid[i][0].state == 1) {
-        grid[i][0].possibilities = grid[i - 1][0].possibilities;
-        grid[i][0].possibilities.push_back(0);
-        grid[i][0].possibilities.back() = addMod(
-            grid[i][0].possibilities.back(),
-            grid[i][0].possibilities[grid[i][0].possibilities.size() - 2]);
-      } else if (grid[i][0].state == 0) {
-        grid[i][0].possibilities = grid[i - 1][0].possibilities;
-      }
-    }
-
-    for (int j = 1; j <= cols; ++j) {
-      if (grid[0][j].state == 1) {
-        grid[0][j].possibilities = grid[0][j - 1].possibilities;
-        grid[0][j].possibilities.push_back(0);
-        grid[0][j].possibilities.back() = addMod(
-            grid[0][j].possibilities.back(),
-            grid[0][j].possibilities[grid[0][j].possibilities.size() - 2]);
-      } else if (grid[0][j].state == 0) {
-        grid[0][j].possibilities = grid[0][j - 1].possibilities;
-      }
-    }
-
-    for (int i = 1; i <= rows; ++i) {
-      for (int j = 1; j <= cols; ++j) {
-        if (grid[i][j].state == 0) {
-          for (int l = 0; l < k + 1; ++l) {
-            grid[i][j].possibilities[l] =
-                addMod(grid[i - 1][j].possibilities[l],
-                       grid[i][j - 1].possibilities[l]);
-          }
-        } else if (grid[i][j].state == 1) {
-          for (int l = 0; l < k + 1; ++l) {
-            grid[i][j].possibilities[l] =
-                addMod(grid[i - 1][j].possibilities[l],
-                       grid[i][j - 1].possibilities[l]);
-          }
-          grid[i][j].possibilities.push_back(0);
-          grid[i][j].possibilities.back() = addMod(
-              grid[i][j].possibilities.back(),
-              grid[i][j].possibilities[grid[i][j].possibilities.size() - 2]);
+        for (int i = 0; i <= rows; ++i) {
+            for (int j = 0; j <= cols; ++j) {
+                grid[i][j].possibilities = deque<int>(k + 1, 0);
+            }
         }
-      }
+
+        for (int i = 0; i < a; ++i) {
+            int x, y;
+            fin >> x >> y;
+            grid[x][y].state = 1;
+        }
+
+        for (int i = 0; i < b; ++i) {
+            int x, y;
+            fin >> x >> y;
+            grid[x][y].state = 2;
+        }
+
+        grid[0][0].possibilities.push_front(1);
+        grid[0][0].possibilities.pop_back();
+
+        for (int i = 1; i <= rows; ++i) {
+            if (grid[i][0].state == 1) {
+                grid[i][0].possibilities = grid[i - 1][0].possibilities;
+                grid[i][0].possibilities.push_front(0);
+                int carry = grid[i][0].possibilities.back();
+                grid[i][0].possibilities.pop_back();
+                grid[i][0].possibilities.back() += carry;
+            } else if (grid[i][0].state == 0) {
+                grid[i][0].possibilities = grid[i - 1][0].possibilities;
+            }
+        }
+
+        for (int j = 1; j <= cols; ++j) {
+            if (grid[0][j].state == 1) {
+                grid[0][j].possibilities = grid[0][j - 1].possibilities;
+                grid[0][j].possibilities.push_front(0);
+                int carry = grid[0][j].possibilities.back();
+                grid[0][j].possibilities.pop_back();
+                grid[0][j].possibilities.back() += carry;
+            } else if (grid[0][j].state == 0) {
+                grid[0][j].possibilities = grid[0][j - 1].possibilities;
+            }
+        }
+
+        for (int i = 1; i <= rows; ++i) {
+            for (int j = 1; j <= cols; ++j) {
+                if (grid[i][j].state == 0) {
+                    deque<int> prev_row = grid[i - 1][j].possibilities;
+                    deque<int> prev_col = grid[i][j - 1].possibilities;
+                    deque<int> possibilities;
+                    for (int l = 0; l < k + 1; ++l) {
+                        possibilities.push_back((prev_row[l] % MOD + prev_col[l] % MOD) % MOD);
+                    }
+                    grid[i][j].possibilities = possibilities;
+                } else if (grid[i][j].state == 1) {
+                    deque<int> prev_row = grid[i - 1][j].possibilities;
+                    deque<int> prev_col = grid[i][j - 1].possibilities;
+                    deque<int> possibilities;
+                    for (int l = 0; l < k + 1; ++l) {
+                        possibilities.push_back((prev_row[l] % MOD + prev_col[l] % MOD) % MOD);
+                    }
+                    int carry = possibilities.back();
+                    possibilities.pop_back();
+                    possibilities.back() += carry;
+                    possibilities.push_front(0);
+                    grid[i][j].possibilities = possibilities;
+                }
+            }
+        }
+
+        fout << grid[rows][cols].possibilities.back() << endl;
     }
 
-    fout << grid[rows][cols].possibilities.back() << endl;
-  }
+    fin.close();
+    fout.close();
 
-  fin.close();
-  fout.close();
-
-  return 0;
+    return 0;
 }
---
+++ Grid/v.cpp
@@ -0,0 +1,98 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+#define MOD 1000000007
+#define POINT 0
+#define PASSPOINT 1
+#define FORBIDDENPOINT 2
+
+using namespace std;
+
+int main() {
+    ifstream fin("0.inp");
+    ofstream fout("0.txt");
+
+    int test_cases;
+    fin >> test_cases;
+
+    for (int t = 0; t < test_cases; ++t) {
+        int rows, cols, a, b, k;
+        fin >> rows >> cols >> a >> b >> k;
+
+
+        vector<vector<int>> state(rows + 1, vector<int>( cols + 1));
+        vector<vector<vector<int>>> grid(rows +1 , vector<vector<int>>(cols + 1, vector<int>( k + 1)));
+
+        for (int i = 0; i < a; ++i) {
+            int x, y;
+            fin >> x >> y;
+            state[x][y] =  PASSPOINT;
+        }
+
+        for (int i = 0; i < b; ++i) {
+            int x, y;
+            fin >> x >> y;
+            state[x][y] = FORBIDDENPOINT;
+        }
+
+        grid[0][0][0] = 1;
+        int kn = 0;
+
+        for (int i = 1; i <= rows; ++i) {
+            if (state[i][0] == POINT) {
+                grid[i][0][kn] = grid[i - 1][0][kn];
+            } else if (state[i][0] == PASSPOINT) {
+                kn++;
+                grid[i][0][kn] = grid[i - 1][0][kn];
+            } else{
+              break;
+            }
+        }
+
+        for (int i = 1; i <= cols; ++i) {
+            if (state[0][i] == POINT) {
+                grid[0][i][kn] = grid[0][i-1][kn];
+            } else if (state[i][0] == PASSPOINT) {
+                kn++;
+                grid[0][i][kn] = grid[0][i-1][kn];
+            } else{
+              break;
+            }
+        }
+
+        for (int i = 1; i <= rows; i++)
+        {
+          for (int v = 1; v <= cols; v++)
+          {
+            if(state[i][v]==POINT){
+              for (int l = 0; l <= k; l++)
+              {
+                  grid[i][v][l] =
+                      (grid[i - 1][v][l] % MOD + grid[i][v - 1][l] % MOD) % MOD;
+              }
+            }else if(state[i][v]==PASSPOINT){
+              for (int l = 0; l <= k; l++)
+              {
+                  grid[i][v][l] =
+                      (grid[i - 1][v][l] % MOD + grid[i][v - 1][l] % MOD) % MOD;
+              }
+              grid[i][v][k - 1] += grid[i][v][k];
+              for (int l = 1; l < k; l++)
+              {
+                  grid[i][v][l] = grid[i][v][l - 1];
+              }
+              grid[i][v][0] = 0;
+            }
+          }
+        }
+        
+
+        fout << grid[rows][cols][k] << endl;
+    }
+
+    fin.close();
+    fout.close();
+
+    return 0;
+}
