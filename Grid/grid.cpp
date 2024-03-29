eb784e9aee468a628893e3648f9989f9eb272f0d
WaiCat <98246479+WaiCat@users.noreply.github.com>
2024-03-30 01:24:19 +0900

    Grid 끝!

--- Grid/grid.cpp
+++ Grid/grid.cpp
@@ -1,4 +1,3 @@
-#include <deque>
 #include <fstream>
 #include <iostream>
 #include <vector>
@@ -7,102 +6,95 @@
 
 using namespace std;
 
-struct Cell {
-    int state;
-    deque<int> possibilities;
-
-    Cell() : state(0) {}
-};
-
 int main() {
-    ifstream fin("2.inp");
-    ofstream fout("2.txt");
+  ifstream fin("grid.inp");
+  ofstream fout("grid.out");
 
-    int test_cases;
-    fin >> test_cases;
+  int testCases;
+  fin >> testCases;
 
-    for (int t = 0; t < test_cases; ++t) {
-        int rows, cols, a, b, k;
-        fin >> rows >> cols >> a >> b >> k;
+  for (int testCase = 0; testCase < testCases; ++testCase) {
+    int x, y, a, b, t;
+    fin >> x >> y >> a >> b >> t;
 
-        vector<vector<Cell>> grid(rows + 1, vector<Cell>(cols + 1));
+    vector<vector<int>> state(x + 1, vector<int>(y + 1));
+    vector<vector<vector<int>>> grid(
+        x + 1, vector<vector<int>>(y + 1, vector<int>(t + 1)));
 
-        for (int i = 0; i <= rows; ++i) {
-            for (int j = 0; j <= cols; ++j) {
-                grid[i][j].possibilities = deque<int>(k + 1, 0);
-            }
-        }
-
-        for (int i = 0; i < a; ++i) {
-            int x, y;
-            fin >> x >> y;
-            grid[x][y].state = 1;
-        }
-
-        for (int i = 0; i < b; ++i) {
-            int x, y;
-            fin >> x >> y;
-            grid[x][y].state = 2;
-        }
-
-        grid[0][0].possibilities.push_front(1);
-        grid[0][0].possibilities.pop_back();
-
-        for (int i = 1; i <= rows; ++i) {
-            if (grid[i][0].state == 1) {
-                grid[i][0].possibilities = grid[i - 1][0].possibilities;
-                grid[i][0].possibilities.push_front(0);
-                int carry = grid[i][0].possibilities.back();
-                grid[i][0].possibilities.pop_back();
-                grid[i][0].possibilities.back() += carry;
-            } else if (grid[i][0].state == 0) {
-                grid[i][0].possibilities = grid[i - 1][0].possibilities;
-            }
-        }
-
-        for (int j = 1; j <= cols; ++j) {
-            if (grid[0][j].state == 1) {
-                grid[0][j].possibilities = grid[0][j - 1].possibilities;
-                grid[0][j].possibilities.push_front(0);
-                int carry = grid[0][j].possibilities.back();
-                grid[0][j].possibilities.pop_back();
-                grid[0][j].possibilities.back() += carry;
-            } else if (grid[0][j].state == 0) {
-                grid[0][j].possibilities = grid[0][j - 1].possibilities;
-            }
-        }
-
-        for (int i = 1; i <= rows; ++i) {
-            for (int j = 1; j <= cols; ++j) {
-                if (grid[i][j].state == 0) {
-                    deque<int> prev_row = grid[i - 1][j].possibilities;
-                    deque<int> prev_col = grid[i][j - 1].possibilities;
-                    deque<int> possibilities;
-                    for (int l = 0; l < k + 1; ++l) {
-                        possibilities.push_back((prev_row[l] % MOD + prev_col[l] % MOD) % MOD);
-                    }
-                    grid[i][j].possibilities = possibilities;
-                } else if (grid[i][j].state == 1) {
-                    deque<int> prev_row = grid[i - 1][j].possibilities;
-                    deque<int> prev_col = grid[i][j - 1].possibilities;
-                    deque<int> possibilities;
-                    for (int l = 0; l < k + 1; ++l) {
-                        possibilities.push_back((prev_row[l] % MOD + prev_col[l] % MOD) % MOD);
-                    }
-                    int carry = possibilities.back();
-                    possibilities.pop_back();
-                    possibilities.back() += carry;
-                    possibilities.push_front(0);
-                    grid[i][j].possibilities = possibilities;
-                }
-            }
-        }
-
-        fout << grid[rows][cols].possibilities.back() << endl;
+    for (int i = 0; i < a; ++i) {
+      int x, y;
+      fin >> x >> y;
+      state[x][y] = 1;
     }
 
-    fin.close();
-    fout.close();
+    for (int i = 0; i < b; ++i) {
+      int x, y;
+      fin >> x >> y;
+      state[x][y] = 2;
+    }
 
-    return 0;
+    grid[0][0][0] = 1;
+    int remainingMoves = 0;
+
+    for (int i = 1; i <= x; ++i) {
+      if (state[i][0] == 0) {
+        grid[i][0][remainingMoves] = 1;
+      } else if (state[i][0] == 1) {
+        remainingMoves++;
+        if (remainingMoves > t) {
+          remainingMoves--;
+        }
+        grid[i][0][remainingMoves] = 1;
+      } else {
+        break;
+      }
+    }
+
+    remainingMoves = 0;
+    for (int i = 1; i <= y; ++i) {
+      if (state[0][i] == 0) {
+        grid[0][i][remainingMoves] = 1;
+      } else if (state[0][i] == 1) {
+        remainingMoves++;
+        if (remainingMoves > t) {
+          remainingMoves--;
+        }
+        grid[0][i][remainingMoves] = 1;
+      } else {
+        break;
+      }
+    }
+
+    for (int i = 1; i <= x; i++) {
+      for (int j = 1; j <= y; j++) {
+        if (state[i][j] == 0) {
+          for (int movesLeft = 0; movesLeft <= t; movesLeft++) {
+            grid[i][j][movesLeft] = (grid[i - 1][j][movesLeft] % MOD +
+                                     grid[i][j - 1][movesLeft] % MOD) %
+                                    MOD;
+          }
+        } else if (state[i][j] == 1) {
+          for (int movesLeft = 0; movesLeft <= t; movesLeft++) {
+            grid[i][j][movesLeft] = (grid[i - 1][j][movesLeft] % MOD +
+                                     grid[i][j - 1][movesLeft] % MOD) %
+                                    MOD;
+          }
+          if (t > 0) {
+            grid[i][j][t - 1] += grid[i][j][t];
+            for (int movesLeft = t; movesLeft > 0; movesLeft--) {
+              grid[i][j][movesLeft] = grid[i][j][movesLeft - 1];
+            }
+            grid[i][j][0] = 0;
+          }
+        }
+      }
+    }
+
+    fout << grid[x][y][t] << endl;
+  }
+
+  fin.close();
+  fout.close();
+
+  return 0;
 }
