f14f9f2ddda663b992323408a8d937a141a30377
WaiCat <98246479+WaiCat@users.noreply.github.com>
2024-05-01 15:07:08 +0900

    Area 끝

--- Area/area.cpp
+++ Area/area.cpp
@@ -1,79 +1,84 @@
-#include <iostream>
-#include <fstream>
 #include <cmath>
-#include <vector>
+#include <fstream>
 #include <iomanip>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
 struct Circle {
-    double x, y, r;
+  double x, y, r;
 };
 
 struct Point {
-    double x, y;
+  double x, y;
 };
 
-vector<Point> intersect(const Circle& c1, const Circle& c2) {
-    vector<Point> points;
-    double d = sqrt(pow(c1.x - c2.x, 2) + pow(c1.y - c2.y, 2));
-    if (d > c1.r + c2.r || d < fabs(c1.r - c2.r) || d == 0) {
-        return points; // No intersection or same circle
-    }
+bool is_inside(const Circle& c, const Point& p) {
+  return sqrt(pow(p.x - c.x, 2) + pow(p.y - c.y, 2)) <= c.r;
+}
 
-    double a = (c1.r * c1.r - c2.r * c2.r + d * d) / (2 * d);
-    double h = sqrt(c1.r * c1.r - a * a);
-    double px = c1.x + a * (c2.x - c1.x) / d;
-    double py = c1.y + a * (c2.y - c1.y) / d;
-
-    // Intersection points
-    double x1 = px + h * (c2.y - c1.y) / d;
-    double y1 = py - h * (c2.x - c1.x) / d;
-    double x2 = px - h * (c2.y - c1.y) / d;
-    double y2 = py + h * (c2.x - c1.x) / d;
-
-    points.push_back({x1, y1});
-    if (x1 != x2 || y1 != y2) {
-        points.push_back({x2, y2});
-    }
+Point intersect(const Circle& c1, const Circle& c2, const Circle& c3) {
+  Point points;
+  double d = sqrt(pow(c1.x - c2.x, 2) + pow(c1.y - c2.y, 2));
+  if (d > c1.r + c2.r || d < fabs(c1.r - c2.r) || d == 0) {
     return points;
+  }
+
+  double a = (c1.r * c1.r - c2.r * c2.r + d * d) / (2 * d);
+  double h = sqrt(c1.r * c1.r - a * a);
+  double px = c1.x + a * (c2.x - c1.x) / d;
+  double py = c1.y + a * (c2.y - c1.y) / d;
+
+  double x1 = px + h * (c2.y - c1.y) / d;
+  double y1 = py - h * (c2.x - c1.x) / d;
+  double x2 = px - h * (c2.y - c1.y) / d;
+  double y2 = py + h * (c2.x - c1.x) / d;
+
+  Point p1 = {x1, y1};
+  Point p2 = {x2, y2};
+  if (is_inside(c3, p1)) {
+    points = p1;
+  }
+  if ((x1 != x2 || y1 != y2) && is_inside(c3, p2)) {
+    points = p2;
+  }
+  return points;
 }
 
 double triangle_area(const Point& p1, const Point& p2, const Point& p3) {
-    return 0.5 * abs(p1.x * (p2.y - p3.y) + p2.x * (p3.y - p1.y) + p3.x * (p1.y - p2.y));
+  return 0.5 * abs(p1.x * (p2.y - p3.y) + p2.x * (p3.y - p1.y) +
+                   p3.x * (p1.y - p2.y));
 }
 
 int main() {
-    ifstream fin("1.inp");
-    ofstream fout("1.txt");
-    int T;
-    fin >> T;
-    fout << fixed << setprecision(2);
+  ifstream fin("area.inp");
+  ofstream fout("area.out");
 
-    while (T--) {
-        Circle circles[3];
-        for (int i = 0; i < 3; ++i) {
-            fin >> circles[i].x >> circles[i].y >> circles[i].r;
-        }
+  int T;
+  fin >> T;
+  fout << fixed << setprecision(2);
 
-        vector<Point> points;
-        for (int i = 0; i < 3; ++i) {
-            for (int j = i + 1; j < 3; ++j) {
-                vector<Point> pts = intersect(circles[i], circles[j]);
-                points.insert(points.end(), pts.begin(), pts.end());
-            }
-        }
-
-        // Assuming points form a valid triangle (for simplification)
-        if (points.size() == 3) {
-            double area = triangle_area(points[0], points[1], points[2]);
-            fout << area << endl;
-        } else {
-            fout << "0.00" << endl;
-        }
+  while (T--) {
+    Circle circles[3];
+    for (int i = 0; i < 3; ++i) {
+      fin >> circles[i].x >> circles[i].y >> circles[i].r;
     }
 
-    fin.close();
-    fout.close();
-    return 0;
+    vector<Point> points;
+    points.push_back(intersect(circles[0], circles[1], circles[2]));
+    points.push_back(intersect(circles[1], circles[2], circles[0]));
+    points.push_back(intersect(circles[2], circles[0], circles[1]));
+
+    if (points.size() == 3) {
+      double area = triangle_area(points[0], points[1], points[2]);
+      fout << area << endl;
+    } else {
+      fout << "0.00" << endl;
+    }
+  }
+
+  fin.close();
+  fout.close();
+  return 0;
 }
