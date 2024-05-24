7737422d06dc597eba616bc8d3c375d175419c7c
WaiCat <98246479+WaiCat@users.noreply.github.com>
2024-05-24 13:39:41 +0900

    show 끝

--- Show/show.cpp
+++ Show/show.cpp
@@ -1,67 +1,136 @@
 #include <iostream>
-#include <vector>
 #include <fstream>
-#include <string>
+#include <vector>
+#include <stack>
+#include <algorithm>
 
 using namespace std;
 
-struct Prediction {
-    int bulb1;
-    char color1;
-    int bulb2;
-    char color2;
-    int bulb3;
-    char color3;
+struct Prediction
+{
+    int bulb1, bulb2, bulb3;
+    char color1, color2, color3;
 };
 
-bool isValid(const vector<char>& bulbs, const vector<Prediction>& predictions) {
-    for (const auto& pred : predictions) {
-        int match_count = 0;
-        if (bulbs[pred.bulb1 - 1] == pred.color1) match_count++;
-        if (bulbs[pred.bulb2 - 1] == pred.color2) match_count++;
-        if (bulbs[pred.bulb3 - 1] == pred.color3) match_count++;
-        if (match_count < 2) return false;
+class TwoSat
+{
+public:
+    TwoSat(int n) : n(n), adj(2 * n), radj(2 * n), assignment(n), visited(2 * n, false), comp(2 * n, -1) {}
+
+    void add_clause(int u, bool u_neg, int v, bool v_neg)
+    {
+        int not_u = u + (u_neg ? n : 0);
+        int not_v = v + (v_neg ? n : 0);
+        u = u + (u_neg ? 0 : n);
+        v = v + (v_neg ? 0 : n);
+        adj[not_u].push_back(v);
+        adj[not_v].push_back(u);
+        radj[v].push_back(not_u);
+        radj[u].push_back(not_v);
     }
-    return true;
-}
 
-bool backtrack(vector<char>& bulbs, const vector<Prediction>& predictions, int index) {
-    if (index == bulbs.size()) {
-        return isValid(bulbs, predictions);
+    bool solve()
+    {
+        for (int i = 0; i < 2 * n; ++i)
+        {
+            if (!visited[i])
+                dfs1(i);
+        }
+        fill(visited.begin(), visited.end(), false);
+        int id = 0;
+        while (!order.empty())
+        {
+            int v = order.top();
+            order.pop();
+            if (!visited[v])
+                dfs2(v, id++);
+        }
+        for (int i = 0; i < n; ++i)
+        {
+            if (comp[i] == comp[i + n])
+                return false;
+            assignment[i] = comp[i] > comp[i + n];
+        }
+        return true;
     }
-    bulbs[index] = 'R';
-    if (backtrack(bulbs, predictions, index + 1)) return true;
-    bulbs[index] = 'B';
-    if (backtrack(bulbs, predictions, index + 1)) return true;
-    return false;
-}
 
-int main() {
-    ifstream infile("show.inp");
-    ofstream outfile("show.out");
-    int test_cases;
-    infile >> test_cases;
+    vector<bool> get_assignment()
+    {
+        return assignment;
+    }
 
-    while (test_cases--) {
-        int n, m;
-        infile >> n >> m;
-        vector<Prediction> predictions(m);
+private:
+    int n;
+    vector<vector<int>> adj, radj;
+    vector<int> comp;
+    vector<bool> visited;
+    stack<int> order;
+    vector<bool> assignment;
 
-        for (int i = 0; i < m; i++) {
-            infile >> predictions[i].bulb1 >> predictions[i].color1 
-                   >> predictions[i].bulb2 >> predictions[i].color2 
-                   >> predictions[i].bulb3 >> predictions[i].color3;
+    void dfs1(int v)
+    {
+        visited[v] = true;
+        for (int u : adj[v])
+        {
+            if (!visited[u])
+                dfs1(u);
+        }
+        order.push(v);
+    }
+
+    void dfs2(int v, int id)
+    {
+        visited[v] = true;
+        comp[v] = id;
+        for (int u : radj[v])
+        {
+            if (!visited[u])
+                dfs2(u, id);
+        }
+    }
+};
+
+int main()
+{
+    ifstream inFile("show.inp");
+    ofstream outFile("show.out");
+
+    int T;
+    inFile >> T;
+
+    while (T--)
+    {
+        int K, N;
+        inFile >> K >> N;
+
+        vector<Prediction> predictions(N);
+        for (int i = 0; i < N; ++i)
+        {
+            inFile >> predictions[i].bulb1 >> predictions[i].color1 >> predictions[i].bulb2 >> predictions[i].color2 >> predictions[i].bulb3 >> predictions[i].color3;
         }
 
-        vector<char> bulbs(n, 'B');  // Initializing all bulbs to 'B'
-        if (backtrack(bulbs, predictions, 0)) {
-            outfile << 1 << endl;
-        } else {
-            outfile << -1 << endl;
+        TwoSat solver(K);
+
+        for (const auto &prediction : predictions)
+        {
+            int b1 = prediction.bulb1 - 1;
+            int b2 = prediction.bulb2 - 1;
+            int b3 = prediction.bulb3 - 1;
+            bool c1 = (prediction.color1 == 'R');
+            bool c2 = (prediction.color2 == 'R');
+            bool c3 = (prediction.color3 == 'R');
+
+            solver.add_clause(b1, !c1, b2, !c2);
+            solver.add_clause(b1, !c1, b3, !c3);
+            solver.add_clause(b2, !c2, b3, !c3);
         }
+
+        bool result = solver.solve();
+        outFile << (result ? 1 : -1) << endl;
     }
 
-    infile.close();
-    outfile.close();
+    inFile.close();
+    outFile.close();
+
     return 0;
 }
