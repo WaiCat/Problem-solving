d988f6277434b9f4e2c425740b07b12c949387f4
WaiCat <98246479+WaiCat@users.noreply.github.com>
2024-06-01 16:27:21 +0900

    Transform 끝

--- Transform/transform.cpp
+++ Transform/transform.cpp
@@ -1,141 +1,77 @@
 #include <iostream>
-#include <fstream>
-#include <vector>
 #include <stack>
-#include <algorithm>
+#include <vector>
+#include <fstream>
 
 using namespace std;
 
-// 이진 트리 노드 구조체
-struct TreeNode {
-    int val;
-    TreeNode* left;
-    TreeNode* right;
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-};
+int nextIndex;
+char parenthesisString[2001];
+int bracketPositions[1001];
+vector<char> outputTree;
 
-// 주어진 inorder 순회 결과로부터 이진 트리를 구성하는 함수
-TreeNode* buildTree(vector<int>& inorder) {
-    stack<TreeNode*> s;
-    TreeNode* root = nullptr;
+void generateTree(int start, int end)
+{
+    if (start == end)
+        return;
 
-    for (int val : inorder) {
-        TreeNode* node = new TreeNode(val);
-
-        if (s.empty()) {
-            root = node;
-        } else {
-            if (val < s.top()->val) {
-                s.top()->left = node;
-            } else {
-                TreeNode* parent = nullptr;
-                while (!s.empty() && val > s.top()->val) {
-                    parent = s.top();
-                    s.pop();
-                }
-                parent->right = node;
-            }
-        }
-
-        s.push(node);
+    int mid;
+    for (mid = start; mid < end; ++mid)
+    {
+        if (bracketPositions[mid] == nextIndex)
+            break;
     }
 
-    return root;
+    nextIndex++;
+    outputTree.push_back('(');
+    generateTree(start, mid);
+    outputTree.push_back(')');
+    generateTree(mid + 1, end);
 }
 
-// 이진 트리를 inorder로 순회하여 결과를 반환하는 함수
-void inorderTraversal(TreeNode* root, vector<int>& result) {
-    if (!root) return;
-
-    inorderTraversal(root->left, result);
-    result.push_back(root->val);
-    inorderTraversal(root->right, result);
-}
-
-// 이진 트리를 preorder로 순회하여 결과를 반환하는 함수
-void preorderTraversal(TreeNode* root, vector<int>& result) {
-    if (!root) return;
-
-    result.push_back(root->val);
-    preorderTraversal(root->left, result);
-    preorderTraversal(root->right, result);
-}
-
-// 이진 트리를 괄호식으로 변환하는 함수
-string treeToParenthesis(TreeNode* root) {
-    if (!root) return "";
-
-    string left = treeToParenthesis(root->left);
-    string right = treeToParenthesis(root->right);
-
-    return "(" + left + ")" + "(" + right + ")";
-}
-
-// 괄호식을 이진 트리로 변환하는 함수
-TreeNode* parenthesisToTree(string& s) {
-    stack<TreeNode*> st;
-    for (char c : s) {
-        if (c == '(') {
-            st.push(nullptr);
-        } else if (c == ')') {
-            TreeNode* right = st.top();
-            st.pop();
-            TreeNode* left = st.top();
-            st.pop();
-            TreeNode* node = new TreeNode(0);
-            node->left = left;
-            node->right = right;
-            st.push(node);
-        }
-    }
-    return st.top();
-}
-
-int main() {
-    ifstream fin("1.inp");
-    ofstream fout("1.txt");
-
-    if (!fin) {
-        cerr << "입력 파일을 열 수 없습니다." << endl;
-        return 1;
-    }
-
-    if (!fout) {
-        cerr << "출력 파일을 열 수 없습니다." << endl;
-        return 1;
-    }
+int main()
+{
+    ifstream fin("transform.inp");
+    ofstream fout("transform.out");
 
     int T;
     fin >> T;
 
-    for (int t = 0; t < T; ++t) {
-        int n;
-        string s;
-        fin >> n >> s;
+    while (T--)
+    {
+        int n, k;
+        fin >> n >> k;
 
-        if (s[0] == '(' || s[0] == ')') {
-            // 주어진 괄호식을 이진 트리로 변환
-            TreeNode* root = parenthesisToTree(s);
-            vector<int> inorder, preorder;
-            inorderTraversal(root, inorder);
-            preorderTraversal(root, preorder);
+        fout << n << ' ';
+        if (k)
+        {
+            fin >> parenthesisString;
 
-            fout << n << " ";
-            for (int val : preorder) {
-                fout << val << " ";
+            stack<int> stackIndices;
+            for (int i = 0, index = 1; i < 2 * n; ++i)
+            {
+                if (parenthesisString[i] == '(')
+                    stackIndices.push(index++);
+                else if (parenthesisString[i] == ')')
+                {
+                    fout << stackIndices.top() << ' ';
+                    stackIndices.pop();
+                }
             }
-            fout << endl;
-        } else {
-            // 주어진 이진 트리를 괄호식으로 변환
-            vector<int> inorder(n);
-            for (int i = 0; i < n; ++i) {
-                fin >> inorder[i];
-            }
-            TreeNode* root = buildTree(inorder);
-            string parenthesis = treeToParenthesis(root);
-
-            fout << parenthesis << endl;
         }
+        else
+        {
+            for (int i = 0; i < n; ++i)
+                fin >> bracketPositions[i];
+
+            nextIndex = 1;
+            outputTree.clear();
+            generateTree(0, n);
+
+            for (char bracket : outputTree)
+                fout << bracket;
+        }
+        fout << '\n';
     }
 
     fin.close();
